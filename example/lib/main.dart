77d0aa81bc9b1c71f01d9b83e788c3a4f2d96d38
srinivasa-dev <sriyadav98@gmail.com>
2023-05-05 15:01:05 +0530

    Added support for custom widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -35,6 +34,24 @@ class _HomeScreenState extends State<HomeScreen> {
   final int _nInitValue = 10;
   int _nCurrentValue = 10;
 
+  final int _cInitValue = 7;
+  int _cCurrentValue = 7;
+
+  final List<Map<String, String>> _countryList = [
+    {'flag': 'assets/argentina.png', 'name': 'Argentina'},
+    {'flag': 'assets/australia.png', 'name': 'Australia'},
+    {'flag': 'assets/brazil.png', 'name': 'Brazil'},
+    {'flag': 'assets/canada.png', 'name': 'Canada'},
+    {'flag': 'assets/cuba.png', 'name': 'Cuba'},
+    {'flag': 'assets/hungary.png', 'name': 'Hungary'},
+    {'flag': 'assets/iceland.png', 'name': 'Iceland'},
+    {'flag': 'assets/india.png', 'name': 'India'},
+    {'flag': 'assets/monaco.png', 'name': 'Monaco'},
+    {'flag': 'assets/south-africa.png', 'name': 'South Africa'},
+    {'flag': 'assets/ukraine.png', 'name': 'Ukraine'},
+    {'flag': 'assets/usa.png', 'name': 'USA'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,79 +67,112 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            WheelSlider(
-              totalCount: _totalCount,
-              initValue: _initValue,
-              onValueChanged: (val) {
-                setState(() {
-                  _currentValue = val;
-                });
-              },
-              hapticFeedbackType: HapticFeedbackType.vibrate,
-              pointerColor: Colors.redAccent,
-            ),
-            Text(
-              _currentValue.toString(),
-              style: const TextStyle(
-                fontSize: 18.0,
-                height: 2.0,
-                fontWeight: FontWeight.w500,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              WheelSlider(
+                totalCount: _totalCount,
+                initValue: _initValue,
+                onValueChanged: (val) {
+                  setState(() {
+                    _currentValue = val;
+                  });
+                },
+                hapticFeedbackType: HapticFeedbackType.vibrate,
+                pointerColor: Colors.redAccent,
               ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            WheelSlider.number(
-              horizontal: false,
-              verticalListHeight: 300.0,
-              perspective: 0.01,
-              totalCount: _nTotalCount,
-              initValue: _nInitValue,
-              unSelectedNumberStyle: const TextStyle(
-                fontSize: 16.0,
-                color: Colors.black54,
+              Text(
+                _currentValue.toString(),
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  height: 2.0,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              selectedNumberStyle: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.redAccent,
+              const SizedBox(
+                height: 30.0,
               ),
-              currentIndex: _nCurrentValue,
-              onValueChanged: (val) {
-                setState(() {
-                  _nCurrentValue = val;
-                });
-              },
-              hapticFeedbackType: HapticFeedbackType.heavyImpact,
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            WheelSlider(
-              totalCount: _totalCount,
-              initValue: _initValue,
-              perspective: 0.01,
-              isVibrate: false,
-              background: Container(
-                width: 300,
-                height: double.infinity,
-                color: Colors.brown,
+              WheelSlider.number(
+                horizontal: false,
+                verticalListHeight: 300.0,
+                perspective: 0.01,
+                totalCount: _nTotalCount,
+                initValue: _nInitValue,
+                unSelectedNumberStyle: const TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black54,
+                ),
+                selectedNumberStyle: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent,
+                ),
+                currentIndex: _nCurrentValue,
+                onValueChanged: (val) {
+                  setState(() {
+                    _nCurrentValue = val;
+                  });
+                },
+                hapticFeedbackType: HapticFeedbackType.heavyImpact,
               ),
-              lineColor: Colors.white,
-              customPointer: const ImageIcon(
-                AssetImage('assets/crosshair.png'),
-                color: Colors.black,
-                size: 35.0,
+              const SizedBox(
+                height: 40.0,
               ),
-              onValueChanged: (val) {
-
-              },
-            ),
-          ],
+              WheelSlider.customWidget(
+                totalCount: _countryList.length,
+                initValue: _cInitValue,
+                isInfinite: false,
+                scrollPhysics: const BouncingScrollPhysics(),
+                children: List.generate(_countryList.length, (index) => Center(
+                  child: Image.asset(
+                    _countryList[index]['flag']!,
+                    scale: _cCurrentValue == index ? null : 1.3,
+                    color: _cCurrentValue == index ? Colors.transparent :Colors.white,
+                    colorBlendMode: BlendMode.color,
+                  ),
+                )),
+                onValueChanged: (val) {
+                  setState(() {
+                    _cCurrentValue = val;
+                  });
+                },
+                hapticFeedbackType: HapticFeedbackType.vibrate,
+                showPointer: false,
+                itemSize: 80,
+              ),
+              Text(
+                _countryList[_cCurrentValue]['name'].toString(),
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  height: 2.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(
+                height: 40.0,
+              ),
+              WheelSlider(
+                totalCount: _totalCount,
+                initValue: _initValue,
+                perspective: 0.01,
+                isVibrate: false,
+                background: Container(
+                  width: 300,
+                  height: double.infinity,
+                  color: Colors.brown,
+                ),
+                lineColor: Colors.white,
+                customPointer: const ImageIcon(
+                  AssetImage('assets/crosshair.png'),
+                  color: Colors.black,
+                  size: 35.0,
+                ),
+                onValueChanged: (val) {},
+              ),
+            ],
+          ),
         ),
       ),
     );
